5cb72b203b44a7db1c2f8046a1fc524eb65305fc
Md. Shohanur Rahaman <80113547+shohanR@users.noreply.github.com>
2023-12-05 01:22:15 +0600

    coin related problem solved

--- money_machine.py
+++ money_machine.py
@@ -1,13 +1,6 @@
 class MoneyMachine:
     CURRENCY = "$"
 
-    COIN_VALUES = {
-        "quarters": 0.25,
-        "dimes": 0.10,
-        "nickles": 0.05,
-        "pennies": 0.01
-    }
-
     def __init__(self):
         self.profit = 0
         self.money_received = 0
@@ -18,36 +11,42 @@ class MoneyMachine:
     
     def process_coins(self):
         # Returns the total calculated from inserted
-        print('''\033[33m
-        We accept the following coins:
-        Quarters ($0.25), dimes ($0.10)
-        nickles ($0.05),  pennies ($0.01)\033[m
-        ''')
 
-        self.money_received =0
-
-        for coin in self.COIN_VALUES:
-            user_input = input(f"How many {coin}? Please: ")
+        while True:
+            print('''\033[33m
+            Enter your money: \033[m
+            ''')
             try:
-                num_coins = int(user_input)
-                if num_coins < 0:
-                    print("Wrong input")
-                    return
-                self.money_received += num_coins * self.COIN_VALUES[coin]
-            except ValueError:
-                print("Wrong input")
-                return 
-        print(f"You have provided: {self.CURRENCY}{self.money_received}")
+                self.money_received = float(input())
+            except:
+                if type(self.money_received) != "float":
+                    print("Wrong input! Try again")
+            else:
+                if self.money_received<=0:
+                    print("Wrong input! Try again")
+                else:
+                    break
         return self.money_received
     
     def make_payment(self, cost):
         # Return True when payment is accepted, False if insufficient
         self.process_coins()
         if self.money_received >= cost:
-            change = round(self.money_received - cost, 2)
-            print(f"Here is {self.CURRENCY}{change} in change.")
+            self.change = round(self.money_received - cost, 2)
             self.profit += cost
             self.money_received = 0
+            return True
+        else:
+            print("Sorry that's not enough money. Money refunded.")
+            self.money_received = 0
+            return False
+    def make_change(self):
+        print(f"Here is {self.CURRENCY}{self.change} in change.")
+
+    def get_change(self, cost):
+        if self.change >= cost:
+            self.change = round(self.change - cost, 2)
+            self.profit += cost
             return True
         else:
             print("Sorry that's not enough money. Money refunded.")
