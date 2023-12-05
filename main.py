700792406ccdbf7c0ec9d12a64c2a9a8202a84df
Md. Shohanur Rahaman <80113547+shohanR@users.noreply.github.com>
2023-12-05 16:54:23 +0600

    optimized main.py
    updated redundancy, enhanced error handling

--- main.py
+++ main.py
@@ -34,31 +34,30 @@ second_repeat = 0
 while is_on:
     welcome()
     options = menu.get_items()
-    flag = 0
+
     try:
-        user_choice = int(input(f"What would you like?\n: "))
-    except:
-        print("Wrong input! Please enter number shown in the menu only!")
-        flag = 1
-    
-    if flag == 1:
+        user_choice = int(input("What would you like?\n: "))
+    except ValueError:
+        print("Wrong input! Please enter a number shown in the menu only!")
         continue
-    elif user_choice<=0 or user_choice>4:
-        print("Please enter 1 to 4 only!")
+
+    if user_choice <= 0 or user_choice > 4:
+        print("Please enter a number between 1 and 4 only!")
     else:
         if user_choice == 4:
             print("\033[31m<<THE END>>\033[m")
             break
         else:
             user_choice = menu.set_items(user_choice)
-            beverage = menu.find_drink(user_choice) # Encapsulates the result
+            beverage = menu.find_drink(user_choice)
             if beverage == 0:
                 break
-            # sufficient_resources = coffee_maker.is_resource_sufficient(beverage)  # TrueFalse result
+
             if repeat_test == 1:
                 sufficient_money = money_machine.get_change(beverage.cost)
             else:
-                sufficient_money = money_machine.make_payment(beverage.cost)  # Encapsulates
+                sufficient_money = money_machine.make_payment(beverage.cost)
+
             if sufficient_money:
                 print("Thank you! Allow us to make your beverage now..")
                 coffee_maker.make_coffee(beverage)
@@ -71,16 +70,16 @@ while is_on:
 
             while True:
                 try:
-                    repeat = int(input("To make another purchase, please enter 1, or 0 to end!\n:"))
-                except:
-                    if type(repeat) != "int":
-                        print("Wrong input! Try again.")
+                    repeat = int(input("To make another purchase, please enter 1, or 0 to end!\n: "))
+                except ValueError:
+                    print("Wrong input! Try again.")
                 else:
-                    if repeat < 0 or repeat > 1:
+                    if repeat not in (0, 1):
                         print("Wrong input! Try again!")
                         continue
                     else:
                         break
+
             if repeat == 0:
                 print("\033[31m<<THE END>>\033[m")
                 break
@@ -90,3 +89,4 @@ while is_on:
 
             if second_repeat == 0 or change == 0:
                 repeat_test = 0
+                
