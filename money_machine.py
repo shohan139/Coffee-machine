d2636c062b50b91265b75349da482021b61485ca
Md. Shohanur Rahaman <80113547+shohanR@users.noreply.github.com>
2023-12-05 16:55:43 +0600

    optimized money_machine.py
    redundancy and error handling updated

--- money_machine.py
+++ money_machine.py
@@ -1,35 +1,30 @@
-class MoneyMachine:
+   class MoneyMachine:
     CURRENCY = "$"
 
     def __init__(self):
         # TODO: we should return this profit somewhere in the code, otherwise it's useless.
         self.profit = 0
         self.money_received = 0
+        self.change = 0
 
     def report(self):
-        # Prints the current profit
         print(f"Money: {self.CURRENCY}{self.profit}")
-    
-    def process_coins(self):
-        # Returns the total calculated from inserted
 
+    def process_coins(self):
         while True:
             try:
-                self.money_received = float(input('''\033[33m
-            Enter your money: \033[m
-            '''))
-            except:
-                if type(self.money_received) != "float":
-                    print("Wrong input! Try again")
+                self.money_received = float(input('\033[33mEnter your money: \033[m'))
+            except ValueError:
+                print("Wrong input! Try again")
+                continue
+
+            if self.money_received <= 0:
+                print("Wrong input! Try again")
             else:
-                if self.money_received<=0:
-                    print("Wrong input! Try again")
-                else:
-                    break
+                break
         return self.money_received
-    
+
     def make_payment(self, cost):
-        # Return True when payment is accepted, False if insufficient
         self.process_coins()
         if self.money_received >= cost:
             self.change = round(self.money_received - cost, 2)
@@ -40,10 +35,9 @@ class MoneyMachine:
             print("Sorry that's not enough money. Money refunded.")
             self.money_received = 0
             return False
+
     def make_change(self):
-        if self.change == 0:
-            print("")
-        else:
+        if self.change != 0:
             print(f"Here is {self.CURRENCY}{self.change} in change.")
 
     def get_change(self, cost):
@@ -55,6 +49,6 @@ class MoneyMachine:
             print("Sorry that's not enough money.")
             self.money_received = 0
             return False
-        
+
     def change_amount(self):
         return self.change
