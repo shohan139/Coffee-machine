01792c8d0e32714e366514b694ad679737f93d8f
Md. Shohanur Rahaman <80113547+shohanR@users.noreply.github.com>
2023-12-08 01:29:45 +0600

    Update money_machine.py

--- money_machine.py
+++ money_machine.py
@@ -1,8 +1,16 @@
 class MoneyMachine:
     CURRENCY = "$"
 
+    COIN_VALUES = {
+        "quarters": 0.25,
+        "dimes": 0.10,
+        "nickels": 0.05,
+        "pennies": 0.01
+    }
+
+    QUARTER_LIMIT = 20
+
     def __init__(self):
-        # TODO: we should return this profit somewhere in the code, otherwise it's useless.
         self.profit = 0
         self.money_received = 0
         self.change = 0
@@ -10,22 +18,39 @@ class MoneyMachine:
     def report(self):
         print(f"Money: {self.CURRENCY}{self.profit}")
 
-    def process_coins(self):
+    def process_coins(self, cost):
         while True:
             try:
-                self.money_received = float(input('\033[33mEnter your money: \033[m'))
+                self.money_received = float(input('\033[33mHow many bucks would you like to enter? : \033[m'))
             except ValueError:
                 print("Wrong input! Try again")
                 continue
 
-            if self.money_received <= 0:
-                print("Wrong input! Try again")
-            else:
-                break
+            if self.money_received <= 0 or self.money_received > 20:
+                print(f"Please enter {self.CURRENCY}1 to {self.CURRENCY}20 only! ")
+                continue
+
+            # If the entered amount is less than the cost, ask for quarters
+            if self.money_received < cost:
+                try:
+                    quarters = int(input(
+                        f'\033[33mHow many quarters (0.25) would you like to enter? (limit: {self.QUARTER_LIMIT}): \033[m'))
+                except ValueError:
+                    print("Wrong input! Try again")
+                    continue
+
+                if quarters < 0 or quarters > self.QUARTER_LIMIT:
+                    print(f"Please enter a non-negative integer less than or equal to {self.QUARTER_LIMIT}.")
+                    continue
+
+                self.money_received += quarters * self.COIN_VALUES["quarters"]
+
+            break
+
         return self.money_received
 
     def make_payment(self, cost):
-        self.process_coins()
+        self.process_coins(cost)
         if self.money_received >= cost:
             self.change = round(self.money_received - cost, 2)
             self.profit += cost
@@ -38,7 +63,7 @@ class MoneyMachine:
 
     def make_change(self):
         if self.change != 0:
-            print(f"Here is {self.CURRENCY}{self.change} in change.")
+            print(f"You have {self.CURRENCY}{self.change} left.")
 
     def get_change(self, cost):
         if self.change >= cost:
