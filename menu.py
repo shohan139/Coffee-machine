7d005cbc2d9d8e6ff9069a659b29b0ad3c6aed90
Md. Shohanur Rahaman <80113547+shohanR@users.noreply.github.com>
2023-12-13 15:58:00 +0600

    Update menu.py
    deleted irrelevant codes

--- menu.py
+++ menu.py
@@ -1,21 +1,16 @@
 class MenuItem:
     # Models each Menu Item
-    def __init__(self, name, water, milk, coffee, cost):
+    def __init__(self, name, cost):
         self.name = name
         self.cost = cost
-        self.ingredients = {
-            "water" : water,
-            "milk" : milk,
-            "coffee" : coffee
-        }
 
 class Menu:
     # Models the Menu with drinks
     def __init__(self):
         self.menu = [
-            MenuItem(name="latte", water = 200, milk = 150, coffee = 24, cost = 2.5),
-            MenuItem(name="espresso", water = 50, milk = 0, coffee = 18, cost = 1.5),
-            MenuItem(name="cappuccino", water = 250, milk = 50, coffee = 24, cost = 3), # coffee recipe in grams
+            MenuItem(name="latte", cost = 2.5),
+            MenuItem(name="espresso", cost = 1.5),
+            MenuItem(name="cappuccino", cost = 3), # coffee recipe in grams
         ]
     
     def set_items(self, item):
